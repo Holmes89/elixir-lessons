7e5beb7dad4c23e41fc3392512b9c31586b445bb
Joel Holmes <holmes89@gmail.com>
2017-06-05 11:27:15 -0400

    hello world

---
+++ hello-world/README.md
@@ -0,0 +1,3 @@
+# Hello World
+
+Simple `Hello World` module, takes a name and will say 'Hello _name_' or no parameter which just returns 'Hello World'
---
+++ hello-world/hello.ex
@@ -0,0 +1,10 @@
+defmodule Hello do
+
+  def hello(name) do
+    IO.puts("Hello #{name}")
+  end
+  def hello do
+    IO.puts("Hello World")
+  end
+
+end
