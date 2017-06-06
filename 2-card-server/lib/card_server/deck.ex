0c468a8f581834d981ad18f5c81ffb67ed181475
Joel Holmes <holmes89@gmail.com>
2017-06-06 10:24:06 -0400

    added some new functions

--- 2-card-server/lib/card_server/deck.ex
+++ 2-card-server/lib/card_server/deck.ex
@@ -13,7 +13,25 @@ defmodule Deck do
         do: {suit, face}
   end
 
-  def shuffle(deck \\ new_deck()) do
+  def new_deck(size) when size <= 52 do
+      new_deck()
+  end
+
+  def new_deck(size) do
+      new_deck(size-52)++new_deck()
+  end
+
+  def new_shuffled_deck do
+      new_deck()
+      |> Enum.shuffle
+  end
+
+  def new_shuffled_deck(size) do
+      new_deck(size)
+      |> Enum.shuffle
+  end
+
+  def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
