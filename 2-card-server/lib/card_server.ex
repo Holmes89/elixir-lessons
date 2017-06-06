a00fa705c59a2d3e112cbeed3d410105c44c6bf0
Joel Holmes <Holmes89@gmail.com>
2017-06-05 21:58:44 -0400

    working on card server

---
+++ 2-card-server/lib/card_server.ex
@@ -0,0 +1,18 @@
+defmodule CardServer do
+  @moduledoc """
+  Documentation for CardServer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CardServer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2-card-server/test/card_server_test.exs
@@ -0,0 +1,8 @@
+defmodule CardServerTest do
+  use ExUnit.Case
+  doctest CardServer
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
